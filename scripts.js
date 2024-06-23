3bbed2ca603410343ba76fad6f51d000ffe831c3
Chris Robbie <133471419+ChrisRobbie5@users.noreply.github.com>
2024-06-23 16:26:02 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 document.querySelector('.connect-wallet').addEventListener('click', () => {
   alert('Connect wallet functionality to be implemented');
 });
+const walletInfo = await magic.user.getInfo();
+const walletType = walletInfo.walletType;
+
+if (walletType === "magic") {
+  await magic.wallet.showUI();
+};
